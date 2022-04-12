c0bb04e73b11cc394855e6a43fbf12013c3c37bf
cbmckni <cbmckni@clemson.edu>
2022-04-12 12:23:14 -0400

    updated chart and improved script

--- scripts/kidney.sh
+++ scripts/kidney.sh
@@ -1,49 +1,16 @@
 #!/bin/bash
 
+NPODS=$1
+INDEX=${HOSTNAME##*-}
+
 mkdir -p /workspace/data && cd /workspace/data
 
 printf '/LIBS/GUID = "%s"\n' `uuidgen` > /root/.ncbi/user-settings.mkfg
 
-cat <<EOF > SRA_IDs.txt
-SRR5139394
-SRR5139395
-SRR5139396
-SRR5139397
-SRR5139398
-SRR5139399
-SRR5139400
-SRR5139401
-SRR5139402
-SRR5139403
-SRR5139404
-SRR5139405
-SRR5139406
-SRR5139407
-SRR5139408
-SRR5139409
-SRR5139410
-SRR5139411
-SRR5139412
-SRR5139413
-SRR5139414
-SRR5139415
-SRR5139416
-SRR5139417
-SRR5139418
-SRR5139419
-SRR5139420
-SRR5139421
-SRR5139422
-SRR5139423
-SRR5139424
-SRR5139425
-SRR5139426
-SRR5139427
-SRR5139428
-SRR5139429
-EOF
+split -l$((`wc -l < $2`/${NPODS})) $2 ${2}.split -da 1
 
 while IFS= read -r line
 do
-time prefetch $line >> out.txt
-done < SRA_IDs.txt
+echo ${line} >> times-${INDEX}.txt
+{ time prefetch $line ; } 2>> times-${INDEX}.txt
+done < ${2}.split${INDEX}
