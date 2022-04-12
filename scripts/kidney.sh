ce2450664c2819d3c1f305a35e05615eebf29c61
cbmckni <cbmckni@clemson.edu>
2022-04-12 13:31:54 -0400

    fixed kidney.sh

--- scripts/kidney.sh
+++ scripts/kidney.sh
@@ -7,10 +7,51 @@ mkdir -p /workspace/data && cd /workspace/data
 
 printf '/LIBS/GUID = "%s"\n' `uuidgen` > /root/.ncbi/user-settings.mkfg
 
-split -l$((`wc -l < $2`/${NPODS})) $2 ${2}.split -da 1
+cat <<EOF > SRA_IDs.txt
+SRR5139394
+SRR5139395
+SRR5139396
+SRR5139397
+SRR5139398
+SRR5139399
+SRR5139400
+SRR5139401
+SRR5139402
+SRR5139403
+SRR5139404
+SRR5139405
+SRR5139406
+SRR5139407
+SRR5139408
+SRR5139409
+SRR5139410
+SRR5139411
+SRR5139412
+SRR5139413
+SRR5139414
+SRR5139415
+SRR5139416
+SRR5139417
+SRR5139418
+SRR5139419
+SRR5139420
+SRR5139421
+SRR5139422
+SRR5139423
+SRR5139424
+SRR5139425
+SRR5139426
+SRR5139427
+SRR5139428
+SRR5139429
+EOF
 
+
+split -l $((`wc -l < SRA_IDs.txt`/2)) SRA_IDs.txt SRA_IDs.txt.split -a 1
+
+LETTER=$(echo ${INDEX} | tr '[0-9]' '[a-j]')
 while IFS= read -r line
 do
 echo ${line} >> times-${INDEX}.txt
-{ time prefetch $line ; } 2>> times-${INDEX}.txt
-done < ${2}.split${INDEX}
+{ time prefetch $line ; } 2>> times-${LETTER}.txt
+done < SRA_IDs.txt.split${LETTER}
